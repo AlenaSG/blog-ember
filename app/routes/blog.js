d98a518292c7d2d8cd8312336df01087469b50e5
Alena Golovina and Anna Timofeeva <alena+anna@>
2017-08-09 16:38:58 -0700

    add bootstrap, detail page andrefactoring

---
+++ app/routes/blog.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('blog', params.blog_id);
+  },
+  actions: {
+      saveBlog3(params) {
+        var newBlog = this.store.createRecord('blog', params);
+        newBlog.save();
+        this.transitionTo('index');
+      },
+      update(blog, params) {
+        Object.keys(params).forEach(function(key) {
+          if(params[key]!==undefined) {
+            blog.set(key,params[key]);
+          }
+    });
+    blog.save();
+    this.transitionTo('index');
+  },
+      destroyBlog(blog) {
+        blog.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
+});
